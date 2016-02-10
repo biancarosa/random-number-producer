4ec6aab7892f8e35f5f50a39e49ce0eae676b6d9
Bianca Rosa <me@biancarosa.com.br>
2016-02-10 15:15:00 -0200

    Adding checks to see if buffer is full and messaging back to the client

--- src/main/java/br/com/biancarosa/producer/Producer.java
+++ src/main/java/br/com/biancarosa/producer/Producer.java
@@ -1,9 +1,6 @@
 package br.com.biancarosa.producer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Random;
@@ -11,6 +8,7 @@ import java.util.Random;
 public class Producer extends Thread {
 
     private String name;
+    static boolean wait = false;
 
     public Producer(String name) {
         this.name = name;
@@ -23,11 +21,34 @@ public class Producer extends Thread {
             PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
             out.println(name + "," + n);
             System.out.println("Number " + n + " sent to buffer");
+
+            //Get the return message from the server
+            InputStream is = echoSocket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String message = br.readLine();
+            System.out.println("Message received from the server : " + message);
+
+            String[] messArgs = message.split("\\?");
+            //full
+            if (messArgs[1].equals("true")) {
+                Producer.wait = true;
+            } else {
+                Producer.wait = false;
+            }
+            if (messArgs[2].equals("false")) {
+                Thread.sleep(6000);
+                System.out.println("try again later");
+                sendNumberToBuffer(n); // try again
+            }
+
             echoSocket.close();
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + host);
         } catch (IOException e) {
             System.err.println("Couldn't get I/O for the connection to " + host);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
@@ -40,7 +61,9 @@ public class Producer extends Thread {
             System.out.println("Number " + n + " generated - sending to buffer");
             sendNumberToBuffer(n);
             try {
-                Thread.sleep(3000);
+                if (Producer.wait) {
+                    Thread.sleep(60000);
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
